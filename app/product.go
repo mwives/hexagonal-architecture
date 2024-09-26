082be36f260c6e87f3c6cccb21619bf27276a607
Ives M. W <ivesmw@gmail.com>
2024-09-26 14:00:47 -0300

    feat: add product and its unit tests

---
+++ app/product.go
@@ -0,0 +1,86 @@
+package app
+
+import (
+	"errors"
+
+	"github.com/asaskevich/govalidator"
+)
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
+
+type ProductInterface interface {
+	IsValid() (bool, error)
+	Enable() error
+	Disable() error
+	GetID() string
+	GetName() string
+	GetPrice() float64
+	GetStatus() string
+}
+
+const (
+	DISABLED = "disabled"
+	ENABLED  = "enabled"
+)
+
+type Product struct {
+	ID     string  `valid:"uuidv4"`
+	Name   string  `valid:"required"`
+	Price  float64 `valid:"float,optional"`
+	Status string  `valid:"required"`
+}
+
+func (p *Product) IsValid() (bool, error) {
+	if p.Status == "" {
+		p.Status = DISABLED
+	}
+	if p.Status != ENABLED && p.Status != DISABLED {
+		return false, errors.New("the status must be either enabled or disabled")
+	}
+	if p.Price < 0 {
+		return false, errors.New("the price must be greater than or equal to zero")
+	}
+
+	_, err := govalidator.ValidateStruct(p)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+func (p *Product) Enable() error {
+	if p.Price <= 0 {
+		return errors.New("the price must be greater than zero")
+	}
+
+	p.Status = ENABLED
+	return nil
+}
+
+func (p *Product) Disable() error {
+	if p.Price > 0 {
+		return errors.New("the price must be zero")
+	}
+
+	p.Status = DISABLED
+	return nil
+}
+
+func (p *Product) GetID() string {
+	return p.ID
+}
+
+func (p *Product) GetName() string {
+	return p.Name
+}
+
+func (p *Product) GetPrice() float64 {
+	return p.Price
+}
+
+func (p *Product) GetStatus() string {
+	return p.Status
+}
---
+++ app/product_test.go
@@ -0,0 +1,96 @@
+package app_test
+
+import (
+	"testing"
+
+	uuid "github.com/google/uuid"
+	"github.com/mwives/hexagonal-architecture/app"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProduct_Enable(t *testing.T) {
+	product := app.Product{}
+	product.Name = "Product 1"
+	product.Price = 10
+	product.Status = app.DISABLED
+
+	err := product.Enable()
+
+	assert.Nil(t, err)
+}
+
+func TestProduct_EnableWithNegativePrice(t *testing.T) {
+	product := app.Product{}
+	product.Name = "Product 1"
+	product.Price = -10
+	product.Status = app.DISABLED
+
+	err := product.Enable()
+
+	assert.NotNil(t, err)
+	assert.Equal(t, "the price must be greater than zero", err.Error())
+}
+
+func TestProduct_Disable(t *testing.T) {
+	product := app.Product{}
+	product.Name = "Product 1"
+	product.Price = 0
+	product.Status = app.ENABLED
+
+	err := product.Disable()
+
+	assert.Nil(t, err)
+}
+
+func TestProduct_DisableWithPositivePrice(t *testing.T) {
+	product := app.Product{}
+	product.Name = "Product 1"
+	product.Price = 10
+	product.Status = app.ENABLED
+
+	err := product.Disable()
+
+	assert.NotNil(t, err)
+	assert.Equal(t, "the price must be zero", err.Error())
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := app.Product{}
+	product.ID = uuid.New().String()
+	product.Name = "Product 1"
+	product.Price = 10
+	product.Status = app.ENABLED
+
+	isValid, err := product.IsValid()
+
+	assert.True(t, isValid)
+	assert.Nil(t, err)
+}
+
+func TestProduct_IsValidWithWrongStatus(t *testing.T) {
+	product := app.Product{}
+	product.ID = uuid.New().String()
+	product.Name = "Product 1"
+	product.Price = 10
+	product.Status = "INVALID_STATUS"
+
+	isValid, err := product.IsValid()
+
+	assert.False(t, isValid)
+	assert.NotNil(t, err)
+	assert.Equal(t, "the status must be either enabled or disabled", err.Error())
+}
+
+func TestProduct_IsValidWithNegativePrice(t *testing.T) {
+	product := app.Product{}
+	product.ID = uuid.New().String()
+	product.Name = "Product 1"
+	product.Price = -10
+	product.Status = app.ENABLED
+
+	isValid, err := product.IsValid()
+
+	assert.False(t, isValid)
+	assert.NotNil(t, err)
+	assert.Equal(t, "the price must be greater than or equal to zero", err.Error())
+}
